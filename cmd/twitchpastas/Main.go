adb7bef74f27823607afb439967a804bd7a262a6
Alex <ravenidze2003@mail.ru>
2024-11-09 02:07:04 +0500

    all right?

--- cmd/twitchpastas/Main.go
+++ cmd/twitchpastas/Main.go
@@ -1,7 +1,23 @@
 package main
 
-import ()
+import (
+	"database/sql"
+	"log"
+)
+
+type DBConfig struct {
+	login    string
+	password string
+}
+
+var users = map[string]DBConfig{}
 
 func main() {
+	users["Ravendza"] = DBConfig{login: "Shtopor", password: "sosal"}
 
+	db, err := sql.Open("sqlite3", "./fiber.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
 }
