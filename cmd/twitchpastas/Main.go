7edbf84b010025d74c65c8e15fb21324b2bd58d5
Alex <ravenidze2003@mail.ru>
2024-11-09 02:16:49 +0500

    test

--- cmd/twitchpastas/Main.go
+++ cmd/twitchpastas/Main.go
@@ -13,7 +13,7 @@ type DBConfig struct {
 var users = map[string]DBConfig{}
 
 func main() {
-	users["Ravendza"] = DBConfig{login: "Shtopor", password: "sosal"}
+	users["Ravendza"] = DBConfig{login: "Shtoporrr", password: "sosal"}
 
 	db, err := sql.Open("sqlite3", "./fiber.db")
 	if err != nil {
